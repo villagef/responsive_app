c6657f6e086616c0428883115164eb50369055d9
villagef <filwyd123@gmail.com>
2020-03-28 20:39:47 +0100

    skills section

--- app.js
+++ app.js
@@ -8,3 +8,35 @@ const changeToOpen = burger.addEventListener("click", () => {
 const changeToClose = navLinks.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 });
+
+const htmlBar = document.querySelector(".bar-html");
+const cssBar = document.querySelector(".bar-css");
+const javascriptBar = document.querySelector(".bar-javaScript");
+const reactBar = document.querySelector(".bar-react");
+
+var tl = new TimelineLite();
+
+tl.fromTo(
+  htmlBar,
+  0.75,
+  { width: `calc(0% - 6px)` },
+  { width: `calc(90% - 6px)`, easy: Power4.easyOut }
+)
+  .fromTo(
+    cssBar,
+    0.75,
+    { width: `calc(0% - 6px)` },
+    { width: `calc(70% - 6px)`, easy: Power4.easyOut }
+  )
+  .fromTo(
+    javascriptBar,
+    0.75,
+    { width: `calc(0% - 6px)` },
+    { width: `calc(80% - 6px)`, easy: Power4.easyOut }
+  )
+  .fromTo(
+    reactBar,
+    0.75,
+    { width: `calc(0% - 6px)` },
+    { width: `calc(60% - 6px)`, easy: Power4.easyOut }
+  );
